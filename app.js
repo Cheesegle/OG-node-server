4dca3c47ed264495b5dbcf61acbdc1be3cc1fc37
collin9ex <36419194+collin9ex@users.noreply.github.com>
2019-10-23 22:43:47 -0400

    Update app.js

--- app.js
+++ app.js
@@ -20,18 +20,15 @@ var app = express();
 var google_analytics_id = process.env.GA_ID || null;
 
 function addGa(html) {
-    if (google_analytics_id) {
-        var ga = [
-            "<script type=\"text/javascript\">",
-            "var _gaq = []; // overwrite the existing one, if any",
-            "_gaq.push(['_setAccount', '" + google_analytics_id + "']);",
-            "_gaq.push(['_trackPageview']);",
-            "(function() {",
-            "  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;",
-            "  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';",
-            "  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);",
-            "})();",
-            "</script>"
+  "  if (google_analytics_id) {",
+        "var ga = [",
+"<!-- Global site tag (gtag.js) - Google Analytics -->",
+"<script async src="https://www.googletagmanager.com/gtag/js?id=UA-139761472-1"></script>",
+"<script>",
+  "function gtag(){dataLayer.push(arguments);}",
+  "gtag('js', new Date());",
+ " gtag('config', 'UA-139761472-1');",
+"</script>"
             ].join("\n");
         html = html.replace("</body>", ga + "\n\n</body>");
     }
